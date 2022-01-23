0d9e5e3c613a074e41df3ba7842e981bfe7fb6bd
fouadbst01 <fouadelbssita@gmail.com>
2022-01-23 21:28:08 +0100

    fix problem in data get

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,25 @@
 var express = require("express");
 var router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-var movies = require("../movies.json");
+//var movies = require("../movies.json");
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send(movies);
+router.get("/", async function (req, res, next) {
+  const { take = 0, skip = 10 } = req.query;
+  const movies = await prisma.movies.findMany({
+    take: parseInt(take),
+    skip: parseInt(skip),
+  });
+  res.send({
+    data: movies,
+    pagination: {
+      count: await prisma.movies.count({}), // Total des enregistrements
+      take: skip,
+      skip: take, // Décalage à partir duquel on prend les  données
+    },
+  });
 });
 
 module.exports = router;
